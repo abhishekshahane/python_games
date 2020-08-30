92d2891a32668aa0a43eab93c9bd7a2d063db98b
theabhishekshahane <38487181+abhishekshahane@users.noreply.github.com>
2020-08-30 17:15:56 +0530

    Update 8ball.py

--- 8ball.py
+++ 8ball.py
@@ -1,6 +1,25 @@
 import random
 print("Magic8ball, v1.0")
-choices = ['It is certain', 'It is decidedly so', 'Without a doubt', 'Yes – definitely', 'You may rely on it', 'As I see it, yes', 'Most likely', 'Outlook good', 'Yes Signs point to yes', 'Reply hazy', 'try again', 'Ask again later', 'Better not tell you now', 'Cannot predict now', 'Concentrate and ask again', 'Dont count on it', 'My reply is no', 'My sources say no', 'Outlook not so good', 'Very doubtful']
+choices = ['It is certain', 
+        'It is decidedly so', 
+        'Without a doubt', 
+        'Yes -  definitely', 
+        'You may rely on it', 
+        'As I see it, yes', 
+        'Most likely', 
+        'Outlook good', 
+        'Yes Signs point to yes',
+        'Reply hazy', 
+        'try again', 
+        'Ask again later',
+        'Better not tell you now', 
+        'Cannot predict now', 
+        'Concentrate and ask again', 
+        'Dont count on it', 
+        'My reply is no', 
+        'My sources say no', 
+        'Outlook not so good', 
+        'Very doubtful']
 randomchoice = random.choice(choices)
 a = input("Enter your name:  ")
 def main(randomchoice):
