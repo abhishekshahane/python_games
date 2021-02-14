be400477663f6a41cdba434ae479390e54ed18f7
theabhishekshahane <38487181+abhishekshahane@users.noreply.github.com>
2021-02-14 15:18:06 +0530

    added ascii

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -2,15 +2,16 @@ from getpass import getpass
 #WARNING: ONE WORD ONLY, will add multiple words in a later update maybe
 #lol this is just to check my python skills on classes 
 class Hangman:
-    def __init__(self,input1):
+    def __init__(self,input1,asciihangman):
         self.input1 = input1
+        self.asciihangman = asciihangman
     def play(self):
         def remove(a,li):
             return [val for val in li if val!=a]
         def check_win(li, input1):
             return ''.join(li)==input1
         if len(self.input1.split())>1:
-            print("Sorry, but multiple words aren't supported at this time!")
+            print("Sorry, but we do not support multiple words yet!")
         else:
             li = [letter for letter in self.input1]
             lia = ['_' for letter in self.input1]
@@ -20,9 +21,9 @@ class Hangman:
             for j in range(len(self.input1)):
                 print(j+1, end=" ")
             print()
-            count = 7
+            count = 0
             #main game loop
-            while count!=0:
+            while count!=7:
                 st = ""
                 a = input("Enter your one letter guess: ")
                 if a in li:
@@ -39,13 +40,62 @@ class Hangman:
                     if check_win(lia,self.input1)==True:
                         break
                 else:
-                    print("Sorry, letter doesn't fit!")
-                    count-=1
-            if count==0:
+                    print(self.asciihangman[count])
+                    count+=1
+            if count==7:
                 print("Sorry, you lost")
             else:
                 print("Nice, you got the word")
-
+asciihangman = HANGMANPICS = ['''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========''']
 input1 = getpass()
-c = Hangman(input1)
+c = Hangman(input1,asciihangman)
 c.play()
